7156bd99c348ba571c6a6733e4c418b6c80cc06d
Frank Simon <f.m.sim@t-online.de>
2018-10-02 10:48:50 +0200

    initial commit

---
+++ assets/images/index.php
@@ -0,0 +1,9 @@
+<?php
+
+
+// Hide directory
+include_once($_SERVER['DOCUMENT_ROOT'] . '/404.php');
+exit();
+
+
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title></title>
+  </head>
+  <body>
+    <h1>Hello world</h1>
+    <!-- scripts -->
+    <script type="text/javascript" src="assets/jquery/jquery-3.3.1.js"></script>
+    <!--<script type="text/javascript" src=".js"></script>-->
+  </body>
+</html>
